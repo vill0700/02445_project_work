384598f98241b08ee8ec3754d2f2e35112d34c9d
SelmaKongstad <s255185@dtu.dk>
2026-06-10 18:29:16 +0200

    add submit

---
+++ submit.sh
@@ -0,0 +1,37 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q gpuv100
+### -- set the job Name -- 
+#BSUB -J My_Application
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4 
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 8:00
+#BSUB -gpu "num=1:mode=exclusive_process"
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u s255185@dtu.dk
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output_%J.out 
+#BSUB -e Output_%J.err 
+
+# here follow the commands you want to execute with input.in as the input file
+cd $HOME
+module purge
+module load python3/3.11.9
+export PYTHONPATH=""
+pip install diffusers transformers accelerate --quiet
+/appl9/python/3.11.9/bin/python3 main.py
